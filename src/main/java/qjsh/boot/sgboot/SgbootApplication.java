d7f24288865de980b1df21f9105b4f485d292c97
susu <lk19931990@163.com>
2019-11-16 15:34:24 +0800

    初始化

---
+++ src/main/java/qjsh/boot/sgboot/SgbootApplication.java
@@ -0,0 +1,13 @@
+package qjsh.boot.sgboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qjsh/boot/sgboot/SgbootApplicationTests.java
@@ -0,0 +1,13 @@
+package qjsh.boot.sgboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
